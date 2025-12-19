571be83cec1a7bf6bac3b6527be444fc15ffcae1
MoNRaSpGit <ju4nrsuarez@gmail.com>
2025-12-19 06:13:29 -0300

    Archivo de productoRoutes

---
+++ src/routes/productos.routes.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { obtenerProductos } from "../controllers/productos.controller.js";
+
+const router = Router();
+
+router.get("/", obtenerProductos);
+
+export default router;
+/*dfdsfdsfds*/
